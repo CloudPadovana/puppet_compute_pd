f17d105e46b30568da7007eed6305f3a810d4497
Cristina Aiftimiei <aiftim@pd.infn.it>
2014-09-21 19:21:56 +0200

    added glusterfs

--- compute_pd/manifests/install.pp
+++ compute_pd/manifests/install.pp
@@ -8,6 +8,27 @@ class compute_pd::install {
 			"openstack-nova-common" ]
   package { $computepackages: ensure => "installed" }
 
+  yumrepo { "glusterfs-epel":
+    baseurl             => "http://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-$::operatingsystemrelease/$::architecture/",
+    descr               => "GlusterFS is a clustered file-system capable of scaling to several petabytes",
+    enabled             => 1,
+    gpgcheck            => 1,
+    gpgkey              => 'http://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/pub.key',
+  }
+
+  yumrepo { "glusterfs-noarch-epel":
+    baseurl             => "http://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-$::operatingsystemrelease/noarch/",
+    descr               => "GlusterFS is a clustered file-system capable of scaling to several petabytes",
+    enabled             => 1,
+    gpgcheck            => 1,
+    gpgkey              => 'http://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/pub.key',
+  }
+
+  package { 'glusterfs-fuse':
+       ensure => 'installed',
+       require => [ Yumrepo["glusterfs-epel"], Yumrepo["glusterfs-noarch-epel"] ]
+  }
+
 #  yumrepo { "EGI_CERT":
 #      baseurl => "http://repository.egi.eu/sw/production/cas/1/current/",
 #      descr => "EGI CERT repos",
